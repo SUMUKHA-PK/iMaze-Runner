b58e38fab928068736d48e497154cb6b4cdc1846
Sumukha Pk <sumukhapk46@gmail.com>
2018-12-03 21:08:44 +0530

    Creating a image capture and sending intent

--- app/src/main/java/com/example/sumukhaspc/gym_workout/MainActivity.java
+++ app/src/main/java/com/example/sumukhaspc/gym_workout/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.sumukhaspc.gym_workout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +25,59 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button Submit_image = (Button) findViewById(R.id.submit);
+
+        Submit_image.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view){
+                if(contact_server()){
+                    //start another activity or display request
+                }
+                else{
+                    //show error message or whatever
+                }
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    private boolean contact_server(){
+        try{
+            String serverAddress = "192.168.43.10";
+            int serverPort = 12345;
+            Socket socket = new Socket(serverAddress,serverPort);
+            OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            bw.write("one");
+            bw.write("/0");
+            bw.write("/0");
+            bw.flush();
+
+            InputStream is = socket.getInputStream();
+            byte[] buffer = new byte[1024];
+            int read;
+            while((read = is.read(buffer)) != -1) {
+                String output = new String(buffer, 0, read);
+                if(output.equals("True"))
+                    return true;
+                else if(output.equals(("False")))
+                    return false;
+            }
+
+
+            socket.close();
+        }
+        catch (java.net.UnknownHostException a)
+        {
+            Log.i("error","java.net.UnknownHostException");
+        }
+        catch (java.io.IOException b){
+            Log.i("error","java.io.IOException");
+        }
+    }
+
+
 
 }
