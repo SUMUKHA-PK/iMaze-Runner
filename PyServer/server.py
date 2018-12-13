8d059665ae9515d242e5c123f5c3219af157c37f
Sumukha Pk <sumukhapk46@gmail.com>
2018-12-14 04:04:21 +0530

    Server completed, opens action window to choose action  to run on submit

--- PyServer/server.py
+++ PyServer/server.py
@@ -6,81 +6,71 @@ import threading
 from db import add_cred
 from db import authenticate as auth
 from time import gmtime,strftime
+import signal
+import random
 
 SERVER_IP="192.168.43.10"
-SERVER_PORT_LOGIN=12346
-SERVER_PORT_FILES=12345
-FILE_SIZE = 8192
+SERVER_PORT = 12345
+SIZE = 4096
+
+cur_threads = []
+
+last_file = ""
 
 class FileThread(threading.Thread):
-    def __init__(self,threadID, name):
+    def __init__(self,threadID, name,conn,addr,data):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-    def run(self):
-        print("imageReceiver server is running\n")
-        server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        serverAddress = SERVER_IP
-        serverPort = SERVER_PORT_FILES
-        try:
-            server_sock.bind((serverAddress,serverPort))
-        except Exception as e:
-            print(e)
+        self.conn = conn
+        self.addr = addr
+        self.data = data
 
-        server_sock.listen(10)
+    def run(self):
+        print("File transfer begins: ")
+        cur_time = str(strftime("%Y-%m-%d %H:%M:%S", gmtime())).replace(" ","_").replace(":","_")
+        file_path = "./images/"+cur_time+".png"
+        last_file = file_path
+        directory = os.path.dirname(file_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        myfile=open(file_path,'wb')
 
         while True:
-            client,addr = server_sock.accept()
-            print("client connected for sending files")
-            cur_time = str(strftime("%Y-%m-%d %H:%M:%S", gmtime())).replace(" ","_").replace(":","_")
-            file_path = "./images/"+cur_time+".png"
-            directory = os.path.dirname(file_path)
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            myfile=open(file_path,'wb')
-            while True:
-                data=client.recv(FILE_SIZE)
-                if not data:
-                    break
-                myfile.write(data)
-            myfile.close()
-            
-            # Open matlab, run the script for this image and get results, send back to client
-            client.close()
-            print("done")
+            if not self.data:
+                break
+            myfile.write(self.data)
+            self.data = self.conn.recv(SIZE)
+        print("Image transferred!")
+        myfile.close()
 
+        self.conn.close()
+        cur_threads.remove(self.threadID)
+        
 class LoginRegisterThread(threading.Thread):
-    def __init__(self,threadID,name):
+
+    def __init__(self,threadID,name,conn,addr,data):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+        self.conn = conn
+        self.addr = addr
+        self.data = data
 
     def run(self):
-        print("handshake server is running\n")
-        server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        serverAddress = SERVER_IP
-        serverPort = SERVER_PORT_LOGIN
-        try:
-            server_sock.bind((serverAddress,serverPort))
-        except Exception as e:
-            print(e)
-        server_sock.listen(10)
-        while True:
-            conn,addr = server_sock.accept()
-            print("client connected for login/register")
-            data = conn.recv(4096)
-            print("DATA received:\n_____________________")
-            print(data)
-            print("_________________________")
-            result = self.process(data)
-            out = str(result).encode()
-            conn.send(out)
-            print(out)
+        print("DATA received:\n_____________________")
+        print(self.data)
+        print("_________________________")
+        result = self.process(self.data)
+        out = str(result).encode()
+        self.conn.send(out)
+        print(out)
+
+        self.conn.close()
+        cur_threads.remove(self.threadID)
+        
 
     def process(self,data):
-    # Basically this must check the hash of the password (currently just match strings and return a value)
         try:
             credentials = data.decode().split("/0")
             if(credentials[0]=="register"):
@@ -94,23 +84,57 @@ class LoginRegisterThread(threading.Thread):
                     result = auth(credentials)
                 except:
                     return False
-                return result        
+                return result
         except Exception as e:
             print("Decoding error in process:",e)
 
+def check_dest(data):
+    try:
+        input = data.decode().split("/0")
+        if((input[0]=="register")|(input[0]=="login")):
+            return "cred"
+    except Exception as e:
+        return "file"
+
+
+def call_thread(threadID,conn,addr):
+    data = conn.recv(SIZE)
+    result = check_dest(data)
+    if(result == "cred"):
+        cred = LoginRegisterThread(threadID,"LR",conn,addr,data)
+        cred.start()
+    elif(result == "file"):
+        files = FileThread(threadID,"FT",conn,addr,data)
+        files.start()
 
 def main():
-    print("Attempting to start the servers\n")
+
+    server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server_sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+
+    try: 
+        server_sock.bind((SERVER_IP,SERVER_PORT))
+    except Exception as e:
+        print(e)
     
-    files = FileThread(1, "FT")
-    loginRegister = LoginRegisterThread(2, "LR")
+    server_sock.listen(10)
 
-    files.start()
-    loginRegister.start()
-    files.join()
-    loginRegister.join()
+    while True :
+        print("Server is listening for incoming connections")
 
-    print("Exiting the main program")
+        conn = None
+        conn,addr = server_sock.accept()
+
+        print("Connection accepted from : ",end=" ")
+        print(addr)
+
+        if(conn != None):
+            thread_no = random.randint(1,1000)
+            while thread_no in cur_threads:
+                thread_no = random.randint(1,1000)
+            cur_threads.append(thread_no)
+            call_thread(thread_no,conn,addr)    
+        print(cur_threads)
 
 if __name__ == "__main__":
     main()
--- app/src/main/java/com/example/sumukhaspc/gym_workout/MainActivity.java
+++ app/src/main/java/com/example/sumukhaspc/gym_workout/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements Imageutils.ImageA
                     }
                 });
                 thread.start();
-               // startActivity(new Intent(MainActivity.this,process.class));
+                startActivity(new Intent(MainActivity.this,process.class));
             }
         });
 
